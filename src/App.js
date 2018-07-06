f9f855096dba4663412e33623002c9d0b5b526a8
Michelle Medina <medina.m1016@gmail.com>
2018-07-06 11:23:49 -0700

    profile picture

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbars from './components/Navbars';
 import './App.css';
 
 
@@ -6,11 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"> </h1>
+      <header>
+          <Navbars />
         </header>
         <p className="App-intro">
-          Welcome to my website! 
+          From Educator to Software Engineer <br />
+          <div className="profile">
+          <img src={require("./image/Michelle.png")} />
+          </div>
         </p>
       </div>
     );
