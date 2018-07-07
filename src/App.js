552fcc9da367bc98f3675505edb83abdf1c06d23
Michelle Medina <medina.m1016@gmail.com>
2018-07-07 13:11:00 -0700

    starter code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Columns from './components/Columns';
 import Navbars from './components/Navbars';
 import './App.css';
 
@@ -7,15 +8,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <header>
+        <header>
           <Navbars />
         </header>
         <p className="App-intro">
-          From Educator to Software Engineer <br />
+          <b>From Educator to Software Engineer </b> <br />
+          <h3 className='subtitle'>a full-stack web developer with a passion for creativity.</h3>
+          <br />
           <div className="profile">
-          <img src={require("./image/Michelle.png")} />
+            <img src={require("./image/michelle2.png")} />
+            <br /><br />
           </div>
         </p>
+        <div className="container-color">
+          <br /><br /><br />
+          <h1> <b> Hi, I’m Michelle Medina - a San Francisco-based, foodie, adventurer, developer and former educator. </b> </h1>
+          <h2>This is where I write a bunch of cool stuff</h2>
+          <br /><br /><br /><br /><br /><br />
+        </div>
+        <Columns />
       </div>
     );
   }
---
+++ src/components/Columns.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Grid, Row, Col} from 'react-bootstrap';
+
+ 
+ 
+class Columns extends React.Component {
+
+    render() {
+      return (
+<div>
+        <Grid className="Grid">
+          <Row className="show-grid">
+            <Col md={6} mdPush={6}>
+              <h1><b>Mentor</b></h1>
+              <h3>I am an Event Lead for a local weekly JavaScript study group through Women Who Code. </h3>
+              <h3 className="Bold"><b>Where I teach: </b></h3>
+              <h4>Women Who Code - Javascript Study Group</h4>
+              <br />
+            </Col>
+            <Col md={6} mdPull={6}>
+              <h1><b>Full-Stack Developer</b></h1>
+              <h3>I enjoy brining ideas to life through software development. </h3>
+              <br />
+              <h3 className="Bold"><b>Languages I Speak: </b></h3>
+              <h4>JavaScript, HTML, CSS</h4>
+              <br />
+              <h3 className="Bold"><b>Dev Tools: </b></h3>
+              <h4>React<br />
+              Bootstrap<br />
+              Git<br />
+              GitHub<br />
+              Terminal<br />
+              Visual Studio Code</h4>
+
+            </Col>
+          </Row>
+        </Grid>
+        </div>
+      )
+    }
+  }
+
+export default Columns;
